b7064d8cdf4983adb481972638fa90d70889470d
Terry Brown <ptbrown514@gmail.com>
2020-04-07 17:03:21 -0500

    first commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>About me!</h1>
+        <p>This is a paragraph that will fill space</p>
+        <p>This is a paragraph that will fill space</p>
+        <p>This is a paragraph that will fill space</p>
+    </>
+);
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello World</h1>
+        <p>This is a paragraph that will fill space</p>
+        <p>This is a paragraph that will fill space</p>
+        <p>This is a paragraph that will fill space</p>
+    </>
+);
+
+export default HomePage;
